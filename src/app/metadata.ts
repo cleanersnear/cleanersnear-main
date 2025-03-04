23b36c20c0cb1a22f9eab1caaa961f17c6f2c4f0
Cleaning Professionals <account@cleaningprofessionals.com.au>
2025-03-05 08:20:57 +1100

    Add files via upload

---
+++ src/app/metadata.ts
@@ -0,0 +1,25 @@
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Professional Cleaners Melbourne | End of Lease & House Cleaning Services',
+  description: '✓ Bond Back Guarantee ✓ Same Day Service ✓ Professional Cleaners. Melbourne\'s #1 rated cleaning service. NDIS registered, fully insured. Get an instant quote online!',
+  alternates: {
+    canonical: 'https://cleaningprofessionals.com.au'
+  },
+  openGraph: {
+    title: 'Professional Cleaners Melbourne | End of Lease & House Cleaning Services',
+    description: '✓ Bond Back Guarantee ✓ Same Day Service ✓ Professional Cleaners. Melbourne\'s #1 rated cleaning service. Book online today!',
+    url: 'https://cleaningprofessionals.com.au',
+    siteName: 'Cleaning Professionals Melbourne',
+    images: [
+      {
+        url: '/images/og-image.jpg',
+        width: 1200,
+        height: 630,
+        alt: 'Professional Cleaning Services Melbourne'
+      }
+    ],
+    locale: 'en_AU',
+    type: 'website'
+  }
+} 
