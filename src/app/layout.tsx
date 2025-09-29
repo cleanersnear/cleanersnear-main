455fe093356f771b44b892daae277cfd64d8021e
Cleaning Professionals <account@cleaningprofessionals.com.au>
2025-09-30 07:59:34 +1000

    google tag rank an co updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -442,7 +442,7 @@ export default function RootLayout({
               new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
               j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
               'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-W87SXGYKC1');
+              })(window,document,'script','dataLayer','GTM-WP9W8MHP');
             `,
           }}
         />
@@ -475,7 +475,7 @@ export default function RootLayout({
         {/* Google Tag Manager (noscript) */}
         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-W87SXGYKC1"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WP9W8MHP"
             height="0"
             width="0"
             style={{ display: 'none', visibility: 'hidden' }}
